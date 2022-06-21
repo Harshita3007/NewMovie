385986f11f7cb7499d531141295567bc07a1e6f9
Harshita3007 <107265105+Harshita3007@users.noreply.github.com>
2022-06-21 12:15:55 +0530

    Update PvtFavourites.jsx

--- src/Components/PvtFavourites.jsx
+++ src/Components/PvtFavourites.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-// import { toast, ToastContainer } from "react-toastify";
+
 import MovieListInFvt from "./MovieListInFvt";
 import Navbar, { pvtMovies } from "./Navbar";
 
@@ -21,16 +21,7 @@ function PvtFavourites() {
           }
         );
         const newMovies = await response.json();
-        // toast.success('Deleted!', {
-        //     position: "bottom-right",
-        //     theme: "dark",
-        //     autoClose: 2000,
-        //     hideProgressBar: false,
-        //     closeOnClick: true,
-        //     pauseOnHover: true,
-        //     draggable: true,
-        //     progress: undefined,
-        //     });
+       
         setMov(newMovies)
 
     }
@@ -43,17 +34,7 @@ function PvtFavourites() {
             <div className="row fvt-row" >
                 <MovieListInFvt movies={mov} handleFavClick={DeleteFromPrivateFav}/>
             </div>
-            {/* <ToastContainer
-                position="bottom-right"
-                autoClose={2000}
-                hideProgressBar={false}
-                newestOnTop
-                closeOnClick
-                rtl={false}
-                pauseOnFocusLoss
-                draggable
-                pauseOnHover
-            /> */}
+      
         </div>
 
 
